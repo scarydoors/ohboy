ee9bb26045ac331634149a8d6f0cdb96ebb813fe
Alan Strauhs <scarydoorsyeah@gmail.com>
2026-04-06 11:45:16 +0100

    feat: read first 2 bytes in hex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs::File, io::Read, path, process::ExitCode};
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    let file = match args.get(1) {
+        Some(s) => {
+            let filepath = path::Path::new(s);
+            File::open(filepath)                    
+        },
+        None => {
+            eprintln!("missing path argument");
+            return ExitCode::from(2);
+        },
+    };
+
+    let mut file = match file {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("unable to open file: {}", err);
+            return ExitCode::FAILURE
+        },
+    };
+
+    let mut next_2_bytes: [u8; 2] = [0; 2];
+    file.read_exact(&mut next_2_bytes).unwrap();
+
+    println!("{:#X}", next_2_bytes.get(0).unwrap());
+    println!("{:#X}", next_2_bytes.get(1).unwrap());
+
+    ExitCode::SUCCESS
 }
