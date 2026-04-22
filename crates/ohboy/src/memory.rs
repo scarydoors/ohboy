e78cf6ba07f71c73552aea64adcfbef0c2c1c3cb
Alan Strauhs <scarydoorsyeah@gmail.com>
2026-04-22 20:20:08 +0100

    feat: add scy and scx support

--- crates/ohboy/src/memory.rs
+++ crates/ohboy/src/memory.rs
@@ -43,11 +43,13 @@ impl<const N: usize, const START: u16> Default for MemoryRegion<N, START> {
 }
 
 type VRam = MemoryRegion<8192, 0x8000>;
-
 type WRam = MemoryRegion<8192, 0xC000>;
 
 const REQUESTED_INTERRUPTS_ADDRESS: u16 = 0xFF0F;
 const ENABLED_INTERRUPTS_ADDRESS: u16 = 0xFFFF;
+
+const SCREEN_Y_ADDRESS: u16 = 0xFF42;
+const SCREEN_X_ADDRESS: u16 = 0xFF43;
 
 pub struct Memory {
     // provides rom bank and stuff
@@ -59,6 +61,9 @@ pub struct Memory {
     // IO registers
     requested_interrupts: Register<interrupt::RequestFlags>,
     enabled_interrupts: Register<interrupt::EnableFlags>,
+
+    screen_y: Register<u8>,
+    screen_x: Register<u8>
 }
 
 impl Memory {
@@ -67,9 +72,13 @@ impl Memory {
             mbc: mbc,
             vram: Default::default(),
             wram: Default::default(),
+            hram: 
 
             requested_interrupts: Default::default(),
-            enabled_interrupts: Default::default()
+            enabled_interrupts: Default::default(),
+
+            screen_y: Default::default(),
+            screen_x: Default::default()
         }
     }
 }
@@ -83,6 +92,8 @@ impl ReadMemory for Memory {
             WRam::START..=WRam::END => self.wram.read_memory(address),
             REQUESTED_INTERRUPTS_ADDRESS => self.requested_interrupts.get().bits(),
             ENABLED_INTERRUPTS_ADDRESS => self.enabled_interrupts.get().bits(),
+            SCREEN_Y_ADDRESS => self.screen_y.get(),
+            SCREEN_X_ADDRESS => self.screen_x.get(),
             _ => unimplemented!("address: {:x}", address)
         }
     }
@@ -97,6 +108,8 @@ impl WriteMemory for Memory {
             WRam::START..=WRam::END => self.wram.write_memory(address, value),
             REQUESTED_INTERRUPTS_ADDRESS => self.requested_interrupts.set(RequestFlags::from_bits_truncate(value)),
             ENABLED_INTERRUPTS_ADDRESS => self.enabled_interrupts.set(EnableFlags::from_bits_truncate(value)),
+            SCREEN_Y_ADDRESS => self.screen_y.set(value),
+            SCREEN_X_ADDRESS => self.screen_x.set(value),
             _ => unimplemented!("address: {:x}", address)
         }
     }
